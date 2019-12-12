8a826110db3bb336861ebf2c18ab4c795e918b92
Tammy Lam <lamtammy1515@gmail.com>
2019-12-12 03:10:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,7 +5,7 @@ def reverse_each_word(rev)
  
   original_array = rev.split(" ")
     return_array = []
-  original_array.each do|rev|
+  original_array.each do |rev|
   return_array << rev.reverse
   end
   return_array.join(" ")
@@ -14,7 +14,7 @@ end
 def reverse_each_word(rever)
   array = rever.split(" ")
      our_array = []
-  array.collect do|rever|
+  array.collect do |rever|
     our_array << string.reverse
 end
     our_array.join(" ")

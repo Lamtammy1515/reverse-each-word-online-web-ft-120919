56ccde7839647bbc262844511a2567c7a9eb961f
Tammy Lam <lamtammy1515@gmail.com>
2019-12-12 03:09:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,23 +6,17 @@ def reverse_each_word(rev)
   original_array = rev.split(" ")
     return_array = []
   original_array.each do|rev|
-    return_array << rev.reverse
-  
+  return_array << rev.reverse
   end
-  
   return_array.join(" ")
-
 end
 
 def reverse_each_word(rever)
-   
   array = rever.split(" ")
      our_array = []
   array.collect do|rever|
     our_array << string.reverse
-    
 end
-
     our_array.join(" ")
     
     
